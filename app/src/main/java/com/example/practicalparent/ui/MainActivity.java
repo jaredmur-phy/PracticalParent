395442cad0c40f2fc70ddb708d0ec942434e2c23
sisik <sisik@sfu.ca>
2020-10-26 22:43:06 -0700

    Added background to mainmenu

--- app/src/main/java/com/example/practicalparent/ui/MainActivity.java
+++ app/src/main/java/com/example/practicalparent/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.practicalparent;
+package com.example.practicalparent.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.practicalparent.R;
 
 public class MainActivity extends AppCompatActivity {
 
