2e4b75b125a2c462d9a4a84afa4cf67d1cdcd9db
sisik <sisik@sfu.ca>
2020-10-28 11:41:22 -0700

    Added heads and tails

--- app/src/main/java/com/example/practicalparent/project/ui/MainActivity.java
+++ app/src/main/java/com/example/practicalparent/project/ui/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.practicalparent.project.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -57,13 +58,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void launchCoinFlip() {
+        Intent intent = CoinFlipActivity.makeLaunchIntent(MainActivity.this);
+        startActivity(intent);
+    }
     private void launchConfigurations() {
         /*Intent intent = ?.makeIntent(MainActivity.this);
-        StartActivity(intent);*/
-    }
-
-    private void launchCoinFlip() {
-       /* Intent intent = ?.makeIntent(MainActivity.this);
         StartActivity(intent);*/
     }
 
