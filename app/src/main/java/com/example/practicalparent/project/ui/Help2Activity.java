f66264e23991c731467e986c2e67926302c567a3
sisik <sisik@sfu.ca>
2020-11-02 15:34:21 -0800

    Changed sound files and added another help page

---
+++ app/src/main/java/com/example/practicalparent/project/ui/Help2Activity.java
@@ -0,0 +1,29 @@
+package com.example.practicalparent.project.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.example.practicalparent.R;
+
+public class Help2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help2);
+    }
+
+    public static Intent makeLaunchIntent(Context context) {
+        Intent intent = new Intent(context, Help2Activity.class);
+        return intent;
+    }
+}
--- app/src/main/java/com/example/practicalparent/project/ui/HelpActivity.java
+++ app/src/main/java/com/example/practicalparent/project/ui/HelpActivity.java
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 import com.example.practicalparent.R;
 
@@ -20,9 +21,24 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
-
+        setNextPage();
     }
 
+    private void setNextPage() {
+    Button buttonNextPage = (Button) findViewById(R.id.idNextPage);
+
+    buttonNextPage.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            launchNextPage();
+        }
+    });
+    }
+
+    private void launchNextPage() {
+        Intent intent = Help2Activity.makeLaunchIntent(HelpActivity.this);
+        startActivity(intent);
+    }
 
     public static Intent makeLaunchIntent(Context context){
         Intent intent = new Intent(context,HelpActivity.class);
