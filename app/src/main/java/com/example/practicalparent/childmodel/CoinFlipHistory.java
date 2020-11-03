baf64e2bc5134ac8caac396705d35194fd403d47
= <jiatang_zhou@sfu.ca>
2020-11-03 00:16:31 -0800

    add CoinFlipHistoryManager and CoinFlipHistoryActivity
    add a page to show the Coin Flipping history and a manager to manage the history

---
+++ app/src/main/java/com/example/practicalparent/childmodel/CoinFlipHistory.java
@@ -0,0 +1,50 @@
+package com.example.practicalparent.childmodel;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CoinFlipHistory {
+
+    private static final String FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    private Child child;
+    private boolean pickedHead;
+    private boolean gotHead;
+    private String date;
+
+    public CoinFlipHistory(boolean pickedHead, boolean gotHead){
+        // deal with no child case
+        this(new Child(""), pickedHead, gotHead);
+    }
+
+    public CoinFlipHistory(Child child, boolean pickedHead, boolean gotHead){
+        this(child, pickedHead, gotHead, new SimpleDateFormat(FORMAT).format(new Date()));
+    }
+
+    CoinFlipHistory(Child child, boolean pickedHead, boolean gotHead, String date){
+        this.child = child;
+        this.pickedHead = pickedHead;
+        this.gotHead = gotHead;
+        this.date = date;
+    }
+
+    public Child getChild() {
+        return child;
+    }
+
+    public boolean isPickedHead() {
+        return pickedHead;
+    }
+
+    public boolean isGotHead() {
+        return gotHead;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public boolean isWon(){
+        return pickedHead == gotHead;
+    }
+}
