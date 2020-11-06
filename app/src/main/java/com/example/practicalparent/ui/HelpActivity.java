3811a533aa2ea23d9de68a5b9f380bd68855a7fd
jaredmur-phy <murphyjared2@gmail.com>
2020-11-05 16:18:44 -0800

    Changed some theme colors, fixed source references page
    TODO: Fix URL for last two sources (school, save)

--- app/src/main/java/com/example/practicalparent/ui/HelpActivity.java
+++ app/src/main/java/com/example/practicalparent/ui/HelpActivity.java
@@ -6,8 +6,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.practicalparent.R;
 
@@ -18,23 +20,8 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
-        setNextPage();
-    }
 
-    private void setNextPage() {
-        Button buttonNextPage = (Button) findViewById(R.id.id_next_page_button);
-
-        buttonNextPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                launchNextPage();
-            }
-        });
-    }
-
-    private void launchNextPage() {
-        Intent intent = Help2Activity.makeLaunchIntent(HelpActivity.this);
-        startActivity(intent);
+        setupHyperLinks();
     }
 
     public static Intent getIntent(Context c) {
@@ -45,4 +32,29 @@ public class HelpActivity extends AppCompatActivity {
         return getIntent(c);
     }
 
+    private void setupHyperLinks() {
+        TextView t1 = (TextView)findViewById(R.id.id_source_coin_flip_text);
+        t1.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView t2 = (TextView)findViewById(R.id.id_source_head_text);
+        t2.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView t3 = (TextView)findViewById(R.id.id_source_help_text);
+        t3.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView t4 = (TextView)findViewById(R.id.id_source_sunset_text);
+        t4.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView t5 = (TextView)findViewById(R.id.id_source_tail_text);
+        t5.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView t6 = (TextView)findViewById(R.id.id_source_timer_text);
+        t6.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView t7 = (TextView)findViewById(R.id.id_source_user_text);
+        t7.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView t8 = (TextView)findViewById(R.id.id_source_coin_sound_text2);
+        t8.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView t9 = (TextView)findViewById(R.id.id_source_ring_text2);
+        t9.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView t10 = (TextView)findViewById(R.id.id_source_font_text2);
+        t10.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView t11 = (TextView)findViewById(R.id.id_source_save_child2);
+        t11.setMovementMethod(LinkMovementMethod.getInstance());
+
+    }
 }
