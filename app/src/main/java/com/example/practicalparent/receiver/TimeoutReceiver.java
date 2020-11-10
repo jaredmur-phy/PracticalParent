b14cfb228b659e71f130fdfa3c931b7362e6a983
jaredmur-phy <murphyjared2@gmail.com>
2020-11-09 17:29:29 -0800

    Notification. Still need to stop ringtone and possibly have it drop down.

--- app/src/main/java/com/example/practicalparent/receiver/TimeoutReceiver.java
+++ app/src/main/java/com/example/practicalparent/receiver/TimeoutReceiver.java
@@ -1,13 +1,19 @@
 package com.example.practicalparent.receiver;
 
 import android.annotation.SuppressLint;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.PowerManager;
 
+import androidx.core.app.NotificationCompat;
+
+import com.example.practicalparent.R;
 import com.example.practicalparent.timer.Alarmer;
 import com.example.practicalparent.timer.TimeInMills;
+import com.example.practicalparent.ui.TimerActivity;
 
 /**
  * receive the broadcast when time out and do the alarming
@@ -23,6 +29,23 @@ public class TimeoutReceiver extends BroadcastReceiver {
         if (TIMEOUT_ACTION.equals(action)) {
             wakeupScreen(context);
             Alarmer.getInstance(context).alarm();
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "1")
+                    .setSmallIcon(R.drawable.tail_38)
+                    .setContentTitle("TIMER COMPLETE")
+                    .setContentText("Finished time out")
+                    .setAutoCancel(true);
+
+            Intent intent2 = TimerActivity.makeLaunchIntent(context);
+            intent2.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent2, PendingIntent.FLAG_UPDATE_CURRENT);
+            builder.setContentIntent(pendingIntent);
+
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(
+                    Context.NOTIFICATION_SERVICE
+            );
+            notificationManager.notify(0, builder.build());
         }
     }
 
