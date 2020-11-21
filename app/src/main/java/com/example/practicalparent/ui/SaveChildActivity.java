55865f8aab4f09ab56672d88f71744e017b59cbf
= <jiatang_zhou@sfu.ca>
2020-11-21 12:57:02 -0800

    merge with Whose's Turn add empty name check

--- app/src/main/java/com/example/practicalparent/ui/SaveChildActivity.java
+++ app/src/main/java/com/example/practicalparent/ui/SaveChildActivity.java
@@ -63,7 +63,6 @@ public class SaveChildActivity extends AppCompatActivity {
         setContentView(R.layout.activity_save_child);
         setToolBar();
 
-        // TODO: check duplicate name
         getChildIndex();
         getChildManager();
 
@@ -140,7 +139,11 @@ public class SaveChildActivity extends AppCompatActivity {
                 String childName = editText.getText().toString();
                 ImageView imageView = findViewById(R.id.id_child_img);
 
-                if(childManager.checkChildName(childName, editChildIndex)){
+                if(childName.trim().isEmpty()){
+                    StyleableToast.makeText(SaveChildActivity.this, getString(R.string.name_is_empty),
+                            R.style.resultToast).show();
+                    return;
+                } else if(childManager.checkChildName(childName, editChildIndex)){
                     // duplicate name
                     StyleableToast.makeText(SaveChildActivity.this, getString(R.string.name_already_exist),
                             R.style.resultToast).show();
