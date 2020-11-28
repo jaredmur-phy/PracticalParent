77168bc88565526601438440e7d6ea594b0f4ee8
= <jiatang_zhou@sfu.ca>
2020-11-28 11:53:43 -0800

    change the way of timer timing

--- app/src/main/java/com/example/practicalparent/ui/TimerActivity.java
+++ app/src/main/java/com/example/practicalparent/ui/TimerActivity.java
@@ -212,8 +212,7 @@ public class TimerActivity extends AppCompatActivity {
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     private void setTimer(int minutes) {
-        long triggerTime = SystemClock.elapsedRealtime() + minutes * TimeInMills.MINUTE.getValue();
-        alarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerTime, timeoutCallback);
+        startBroadcast();
         timer.reset(minutes * TimeInMills.MINUTE.getValue());
     }
 
@@ -241,7 +240,7 @@ public class TimerActivity extends AppCompatActivity {
             return;
         }
         timer.resume();
-        alarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP, timer.getEndTime(), timeoutCallback);
+        startBroadcast();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -250,8 +249,16 @@ public class TimerActivity extends AppCompatActivity {
         pauseTimer();
         timer.reset(countDownMinutes * TimeInMills.MINUTE.getValue());
         timer.setPauseStatus();
-        alarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP, timer.getEndTime(), timeoutCallback);
+        startBroadcast();
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void startBroadcast(){
+        alarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime() + TimeInMills.SECOND.getValue()
+                , timeoutCallback);
+    }
+
 
     private String getShowTime() {
         long times = timer.getRemainingTime();
