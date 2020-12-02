abb2f7d104a82a6c76b53d00c176b9bd2508e3f5
= <jiatang_zhou@sfu.ca>
2020-12-02 14:11:25 -0800

    add state machine

--- app/src/main/java/com/example/practicalparent/ui/TakeBreathActivity.java
+++ app/src/main/java/com/example/practicalparent/ui/TakeBreathActivity.java
@@ -1,18 +1,9 @@
 package com.example.practicalparent.ui;
 
-import android.annotation.SuppressLint;
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -20,42 +11,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.practicalparent.R;
-import com.muddzdev.styleabletoastlibrary.StyleableToast;
 
 public class TakeBreathActivity extends AppCompatActivity {
-    private AlertDialog.Builder dialogBuilder;
-    private AlertDialog dialog;
-    private CheckBox checkBox;
-    private Button buttonBreath;
-    private Handler handler = new Handler();
-    private final static int THREE_SECONDS = 3000;
-    private final static int TEN_SECONDS = 13000;
+
+    private final State stateReady = new StateReady();
+    private final State stateWaitToInhale = new StateWaitToInhale();
+    private final State stateInhaling = new StateInhaling();
+    private final State stateOut = new StateOut();
+    private final State stateDoneInhaling = new StateDoneInhaling();
+    private final State stateReadyToExhale = new StateReadyToExhale();
+    private final State stateExhaling = new StateExhaling();
+    private final State stateFinish = new StateFinish();
+
+    private State currentState = stateReady;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_breath);
         setToolBar();
-        setBeginBreath();
-        setChooseBreath();
-
-    }
-
-    private void setChooseBreath() {
-        TextView chooseBreath = (TextView) findViewById(R.id.id_take_N_Breaths);
-
-        chooseBreath.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
- /*dialogBuilder = new AlertDialog.Builder(TakeBreathActivity.this);
-                final View changePopup = getLayoutInflater().inflate(R.layout.radio_popup, null);
-                dialogBuilder.setView(changePopup);
-                dialog = dialogBuilder.create();
-                dialog.show();
-                chooseBreath.setClickable(false);
-                chooseBreath.setText();*/
-            }
-        });
     }
 
     private void setToolBar() {
@@ -66,81 +43,6 @@ public class TakeBreathActivity extends AppCompatActivity {
     }
 
 
-    @SuppressLint("ClickableViewAccessibility")
-    private void setBeginBreath() {
-        buttonBreath = (Button) findViewById(R.id.id_begin_button);
-
-                buttonBreath.setOnTouchListener(new View.OnTouchListener() {
-
-                    @Override
-                    public boolean onTouch(View arg0, MotionEvent arg1) {
-                        switch (arg1.getAction()) {
-                            case MotionEvent.ACTION_DOWN:
-                                buttonBreath.setText(getString(R.string.in));
-                                // start inhale animation and music
-                                //change sound
-                                final MediaPlayer soundEffect = MediaPlayer.create(TakeBreathActivity.this, R.raw.coinflipsound);
-                                StyleableToast.makeText(TakeBreathActivity.this, getString(R.string.breath_in),
-                                        R.style.inhaleToast).show();
-
-                                handler.postDelayed(forThreeSeconds, THREE_SECONDS);
-
-                                /*if(){
-
-                                // 7 more seconds stop exhale animation and music
-                                buttonBreath.setText(getString(R.string.in));
-                                //update remaining breaths
-                                // allow button to be pressed
-                                buttonBreath.setClickable(true);
-
-
-                                } else{
-
-                                 // 7 more seconds stop exhale animation and music
-                                //update remaining breaths to 0
-                                buttonBreath.setText("Good job");
-                                //lock button
-                                buttonBreath.setClickable(false);
-                                }
-                                 */
-
-
-                                handler.postDelayed(forTenSeconds, TEN_SECONDS);
-                                break;
-
-                            default:
-                                handler.removeCallbacks(forThreeSeconds);
-                                break;
-                        }
-                        return true;
-                    }
-                });
-
-            }
-
-    Runnable forThreeSeconds = new Runnable() {
-
-        @Override
-        public void run() {
-            // stop inhale animation and music
-            // start exhale animation and music?
-            buttonBreath.setText(getString(R.string.out));
-
-        }
-    };
-
-    Runnable forTenSeconds = new Runnable() {
-
-        @Override
-        public void run() {
-// Turn off the music and animation
-            // Delete this text
-            buttonBreath.setText("stop");
-            StyleableToast.makeText(TakeBreathActivity.this, getString(R.string.breath_out),
-                    R.style.exhaleToast).show();
-
-        }
-    };
 
     public static Intent getIntent(Context c) {
         return new Intent(c, TakeBreathActivity.class);
@@ -155,6 +57,111 @@ public class TakeBreathActivity extends AppCompatActivity {
         finish();
         return super.onOptionsItemSelected(item);
     }
+
+    private void setStates(State state){
+        currentState = state;
+    }
+
+
+
+    private abstract class State{
+        void onClick(){}
+        void onButtonRelease(){}
+        void onButtonHeld(){}
+        void onButtonHeld3s(){}
+        void onButtonHeld7s(){}
+        void onButtonHeld10s(){}
+        void onButtonNotPressed(){}
+        void onButtonNotPressed3s(){}
+        void onButtonNotPressed7s(){}
+
+        void onMoreBreathNeeded(){ }
+        void noMoreBreathNeeded(){ }
+
+    }
+
+    // TODO: add implementation for those functions.
+
+    private class StateReady extends State{
+        @Override
+        void onClick() {
+            setStates(stateWaitToInhale);
+        }
+    }
+
+    private class StateWaitToInhale extends State{
+        @Override
+        void onButtonHeld() {
+            setStates(stateInhaling);
+        }
+    }
+
+    private class StateInhaling extends State{
+        @Override
+        void onButtonRelease() {
+            setStates(stateWaitToInhale);
+        }
+
+        @Override
+        void onButtonHeld3s() {
+            setStates(stateOut);
+        }
+    }
+
+    private class StateOut extends State{
+        @Override
+        void onButtonRelease() {
+            setStates(stateDoneInhaling);
+        }
+
+        @Override
+        void onButtonHeld10s() {
+            setStates(stateDoneInhaling);
+        }
+    }
+
+    private class StateDoneInhaling extends State{
+        @Override
+        void onButtonNotPressed() {
+            setStates(stateReadyToExhale);
+        }
+    }
+
+    private class StateReadyToExhale extends State{
+        @Override
+        void onButtonNotPressed3s() {
+            setStates(stateExhaling);
+        }
+    }
+
+    private class StateExhaling extends State{
+        @Override
+        void onClick() {
+            setStates(stateFinish);
+        }
+
+        @Override
+        void onButtonNotPressed7s() {
+            setStates(stateFinish);
+        }
+    }
+
+
+    private class StateFinish extends State{
+        @Override
+        void onMoreBreathNeeded() {
+            setStates(stateWaitToInhale);
+        }
+
+        @Override
+        void noMoreBreathNeeded() {
+            // TODO
+        }
+    }
+
+
+
+
 
 
 }
