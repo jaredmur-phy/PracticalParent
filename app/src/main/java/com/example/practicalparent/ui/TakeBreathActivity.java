8b3a47fd0b6626deb219cbdb1d18c6510402404b
sisik <sisik@sfu.ca>
2020-12-06 20:01:24 -0800

    Job done

--- app/src/main/java/com/example/practicalparent/ui/TakeBreathActivity.java
+++ app/src/main/java/com/example/practicalparent/ui/TakeBreathActivity.java
@@ -131,7 +131,6 @@ public class TakeBreathActivity extends AppCompatActivity {
         }
     };
 
-
     private void stopInHalingAnimation() {
         button.clearAnimation();
         if (inHalingPlayer.isPlaying()) {
